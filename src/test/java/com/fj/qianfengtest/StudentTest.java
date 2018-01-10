ec2593372d61bbe0240908567a155f9dd585810b
fangjunaixuexi <1757751686@qq.com>
2018-01-10 19:50:35 +0800

    DI注入

--- src/test/java/com/fj/qianfengtest/StudentTest.java
+++ src/test/java/com/fj/qianfengtest/StudentTest.java
@@ -6,9 +6,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.fj.qianfeng.Student;
 
 public class StudentTest {
-	
+//去除黄线
+@SuppressWarnings("resource")
  @Test
  public void test1() {
+	 
 	 //初始化IOC容器
 	 ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 	 Student stu = (Student) context.getBean("student");
