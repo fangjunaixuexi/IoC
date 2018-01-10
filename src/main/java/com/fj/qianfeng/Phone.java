0ee3c6b2f6b468e1963ca180ed9a2fe6180da149
fangjunaixuexi <1757751686@qq.com>
2018-01-10 20:10:38 +0800

    添加手机功能

---
+++ src/main/java/com/fj/qianfeng/Phone.java
@@ -0,0 +1,23 @@
+package com.fj.qianfeng;
+
+public class Phone {
+	private Integer id;
+	private String name;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "phone [id=" + id + ", name=" + name + "]";
+	}
+
+}
--- src/main/java/com/fj/qianfeng/Student.java
+++ src/main/java/com/fj/qianfeng/Student.java
@@ -3,6 +3,17 @@ package com.fj.qianfeng;
 public class Student {
 	private String name;
 	private Integer age;
+	private Phone phone;
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", age=" + age + ", phone=" + phone + "]";
+	}
+	public Phone getPhone() {
+		return phone;
+	}
+	public void setPhone(Phone phone) {
+		this.phone = phone;
+	}
 	public String getName() {
 		return name;
 	}
@@ -16,15 +27,12 @@ public class Student {
 		this.age = age;
 	}
 	public Student() {
-		System.out.println("wojjjj");
+		System.out.println("进入student实体类");
 	}
 	public Student(String name, Integer age) {
 		this.name = name;
 		this.age = age;
 	}
-	@Override
-	public String toString() {
-		return "Student [name=" + name + ", age=" + age + "]";
-	}
+
 	
 }
